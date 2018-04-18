f9bb39ed224fd01184caea787ae5fee951a92c2f
Kontowicz <37226079+Kontowicz@users.noreply.github.com>
2018-04-18 19:48:47 +0200

    update

--- 27.pl
+++ 27.pl
@@ -2,3 +2,19 @@ otoczenie([Glowa|Lista],Element,Pierwsza,Druga):-
 	otoczenie(Lista,Element,Pierwsza,Druga),!.
 	
 otoczenie([Pierwsza,Element,Druga|_],Element,Pierwsza,Druga).
+
+/*
+o(Lista,Ele,X,Y):-
+	poczatek(Lista,Ele,X),
+	koniec(Lista,Ele,Y).
+	
+poczatek([A1,Ele|_],Ele,A1):-!.
+
+poczatek([Glowa|Ogon],Ele,W):-
+	poczatek(Ogon,Ele,W).
+	
+koniec([Ele,A1|_],Ele,A1):-!.
+
+koniec([Glowa|Ogon],Ele,W):-
+	koniec(Ogon,Ele,W).
+*/
